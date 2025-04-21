a9927c6c6f35278204b829354c63e551e645b5d0
JaimeCastanedo <mordis0812@alumnos.alborfp.com>
2025-04-21 09:52:50 +0200

    Update contacto.js

--- contacto.js
+++ contacto.js
@@ -101,7 +101,7 @@ const contactData = {
         ]
     },
     map: {
-        src: 'https://www.google.com/maps/place/Ctra.+Cortes,+16,+11570+La+Barca+de+la+Florida,+C%C3%A1diz/@36.6492115,-5.9325225,911m/data=!3m1!1e3!4m6!3m5!1s0xd0dba8e5b64a75f:0xfb0b87251fd0c03b!8m2!3d36.6489187!4d-5.932509!16s%2Fg%2F11c19k3xfd?authuser=0&entry=ttu&g_ep=EgoyMDI1MDQxNi4xIKXMDSoJLDEwMjExNjM5SAFQAw%3D%3D',
+        src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3201.0634994287684!2d-5.935083924704173!3d36.648922976213356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0dba8e5b64a75f%3A0xfb0b87251fd0c03b!2sCtra.%20Cortes%2C%2016%2C%2011570%20La%20Barca%20de%20la%20Florida%2C%20C%C3%A1diz!5e0!3m2!1ses!2ses!4v1745221531930!5m2!1ses!2ses',
         title: 'Ubicación de IvánLoraFisio'
     },
 
